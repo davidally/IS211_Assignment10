63de778aab47c3b9a47cb3ab89eb10d2b902ad68
david <davidally94@gmail.com>
2019-04-13 21:58:41 -0400

    Typo fix

--- query_pets.py
+++ query_pets.py
@@ -24,7 +24,7 @@ def main():
                 if user_input == -1:
                     sys.exit()
 
-                elif isinstance(user_input, int) and user_input not in [0, -1]:
+                elif isinstance(user_input, int) and user_input is not -1:
                     cursor.execute(
                         '''
                             SELECT person.first_name, person.last_name, person.age, pet.name, pet.breed, pet.age, pet.dead
@@ -36,23 +36,20 @@ def main():
 
                     # Raise exception if there is no match.
                     requested_data = cursor.fetchall()
-                    if len(requested_data) <= 0:
-                        raise Exception('ERROR: There is no such ID.')
-                    else:
-                        print '''\n{} {} is {} years old.\nPets that belong to this person:\n
-                        '''.format(
-                            requested_data[0][0],
-                            requested_data[0][1],
-                            requested_data[0][2]
-                        )
+                    print '''\n{} {} is {} years old.\nPets that belong to this person:
+                    '''.format(
+                        requested_data[0][0],
+                        requested_data[0][1],
+                        requested_data[0][2]
+                    )
 
-                        for row in requested_data:
-                            if row[6] == 1:
-                                print '{} was a {} who died at the age of {}.'.format(
-                                    row[3], row[4], row[5])
-                            elif row[6] == 0:
-                                print '{}, a {} that is {} years old.'.format(
-                                    row[3], row[4], row[5])
+                    for row in requested_data:
+                        if row[6] == 1:
+                            print '{} was a {} who died at the age of {}.'.format(
+                                row[3], row[4], row[5])
+                        elif row[6] == 0:
+                            print '{}, a {} that is {} years old.'.format(
+                                row[3], row[4], row[5])
 
             except Exception:
                 print 'ERROR: Enter a valid ID.'
